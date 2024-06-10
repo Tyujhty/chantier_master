d25cb3d5c54e76bcb3e68df1eb51adba651b2a07
Fanny BAQUEY <fanny.baquey@protonmail.com>
2024-06-10 19:37:55 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-orange-400">Hello</h1>
+      <p>This is a test</p>
+    </>
+  );
+}
+
+export default App;
